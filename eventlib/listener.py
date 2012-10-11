e2d756e06c6b482305a4e277b9e75372c58a9665
Lincoln de Sousa <lincoln@comum.org>
2012-10-11 16:07:05 -0400

    Adding some missing license headers

--- eventlib/listener.py
+++ eventlib/listener.py
@@ -1,3 +1,18 @@
+# eventlib - Copyright (c) 2012  Yipit, Inc
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from eventlib.core import process_external, import_event_modules
 from eventlib.ejson import loads
 from eventlib.util import redis_connection
