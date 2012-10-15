c3cda9060c512f1847ffa566705587f5a28ce6f7
Steve Pulec <spulec@gmail.com>
2012-10-15 12:48:57 -0400

    0.0.4

--- eventlib/__init__.py
+++ eventlib/__init__.py
@@ -20,7 +20,7 @@ import eventlib.serializers               # pyflakes:ignore
 from .api import log, handler, external_handler, BaseEvent  # pyflakes: ignore
 
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 __all__ = (
     'BaseEvent', 'handler', 'external_handler', 'log',
