1dddb2df7298ac8e3db3baa35a705c9376c6196d
Gabriel Falcao <gabriel@nacaolivre.org>
2013-01-17 10:39:07 -0500

    Releasing 0.0.7
         0000             0000        7777777777777777/========___________
       00000000         00000000      7777^^^^^^^7777/ || ||   ___________
      000    000       000    000     777       7777/=========//
     000      000     000      000             7777// ((     //
    0000      0000   0000      0000           7777//   \\   //
    0000      0000   0000      0000          7777//========//
    0000      0000   0000      0000         7777
    0000      0000   0000      0000        7777
     000      000     000      000        7777
      000    000       000    000       77777
       00000000         00000000       7777777
         0000             0000        777777777

--- eventlib/__init__.py
+++ eventlib/__init__.py
@@ -20,7 +20,7 @@ import eventlib.serializers               # pyflakes:ignore
 from .api import log, handler, external_handler, BaseEvent  # pyflakes: ignore
 
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 __all__ = (
     'BaseEvent', 'handler', 'external_handler', 'log',
