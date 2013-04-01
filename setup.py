7e1cdfda33e526e69164c8c58ada8e56c5c91ef5
Lincoln de Sousa <lincoln@comum.org>
2013-04-01 11:13:26 -0400

    Updating infrastructure files

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-# eventlib - Copyright (c) 2012  Yipit, Inc
+# eventlib - Smart/Async event logging library
+#
+# Copyright (c) 2012-2013  Lincoln Clarete <lincoln@comum.org>
+# Copyright (c) 2012-2013  Yipit, Inc <coders@yipit.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,7 +16,44 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import re
 from setuptools import setup, find_packages
+
+
+def parse_requirements():
+    """Rudimentary parser for the `requirements.txt` file
+
+    We just want to separate regular packages from links to pass them to the
+    `install_requires` and `dependency_links` params of the `setup()`
+    function properly.
+    """
+    try:
+        requirements = \
+            map(str.strip, local_file('requirements.txt').split('\n'))
+    except IOError:
+        raise RuntimeError("Couldn't find the `requirements.txt' file :(")
+
+    links = []
+    pkgs = []
+    for req in requirements:
+        if not req:
+            continue
+        if 'http:' in req or 'https:' in req:
+            links.append(req)
+            name, version = re.findall("\#egg=([^\-]+)-(.+$)", req)[0]
+            pkgs.append('{0}=={1}'.format(name, version))
+        else:
+            pkgs.append(req)
+
+    return pkgs, links
+
+
+local_file = lambda f: \
+    open(os.path.join(os.path.dirname(__file__), f)).read()
+
+
+install_requires, dependency_links = parse_requirements()
 
 
 if __name__ == '__main__':
@@ -22,10 +62,13 @@ if __name__ == '__main__':
         version='0.0.9',
         description=(
             u'Library to make it easy to track events in python/django apps'),
+        long_description=local_file('README.md'),
         author=u'Lincoln de Sousa',
         author_email=u'lincoln@yipit.com',
         url='https://github.com/Yipit/eventlib',
-        packages=find_packages(),
+        packages=filter(lambda n: not n.startswith('tests'), find_packages()),
+        install_requires=install_requires,
+        dependency_links=dependency_links,
         entry_points={
             'console_scripts': [
                 'eventlib = eventlib.runner:main',
